de4e536cef37ae6c0ca96e5369aeb61492b9321a
Victory Emmanuel <115421155+Victory-emmanuel@users.noreply.github.com>
2024-06-12 18:28:15 +0000

    10th commit

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,16 +1,33 @@
 import BlogContent from "../components/blogContent";
-const Blog = () => {
-  return (
-    <>
-      <div id="blog">
-        <div className="w-full bg-extraClr py-24">
-          <div className="max-w-[1240px] mx-auto">
-            <BlogContent />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+   import useFetch from "../hooks/useFetch";
 
-export default Blog;
+   const Blog = () => {
+     const result = useFetch(
+       `https://fuzzy-funicular-x76xpxgx5jpcpr96-1337.app.github.dev/api/blogs?populate=*`
+     );
+
+     if (!result) { 
+       return <p>Initializing...</p>; // Or a loading spinner
+     }
+
+     const { loading, error, data } = result; 
+
+     if (loading) return <p>Loading...</p>;
+     if (error) return <p>Error: {error.message}</p>;
+
+     console.log(data);
+
+     return (
+       <>
+         <div id="blog">
+           <div className="w-full bg-extraClr py-24">
+             <div className="max-w-[1240px] mx-auto">
+               <BlogContent data={data} /> {/* Pass data to BlogContent */}
+             </div>
+           </div>
+         </div>
+       </>
+     );
+   };
+
+   export default Blog;
